56cee7522df71199ce75fc5145a8606eabdad56d
coding-geek07 <exploringtechiegeek@gmail.com>
2025-04-08 16:39:21 +0530

    React Router: Navigate Programatically

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../App.css";
+import { useNavigate } from "react-router";
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("/");
+  };
   return (
     <>
       <Navbar />
       <div className="container">Error Occured!!!</div>
+      <button onClick={navigateHandler}>Navigate to Home</button>
     </>
   );
 };
